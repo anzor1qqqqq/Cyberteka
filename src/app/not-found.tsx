0d112ba0a2a0ab7f7dc25d9646b576a751e01fea
Anzor <anzor.shadzhe@gmail.com>
2023-09-09 14:07:56 +0300

    create error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 
+import ButtonBookUI from '@/UI/buttonBook'
+
 import ErrorSVG from '@/svg/backgroundError/error'
+import ArrowButtomBtnSVG from '@/svg/errorInput/arrowBottomBtn'
+import ArrowLeftBtnSVG from '@/svg/errorInput/arrowLeftBtn'
 
 import '../style/error.css'
 
@@ -16,6 +20,7 @@ export default function NotFound() {
           <div className='contant_404-icon'>
             <span className='number_fouth'>4</span>
             <Image 
+            className='error_icon'
             src={'/static/img/errorIcon/errorIcon.png'}
             width={300}
             height={300}
@@ -32,7 +37,9 @@ export default function NotFound() {
 
         <div className='container_form_contact'>
           <div className='contant_form_contact'>
-            <h1 className='title_form'>Забронируй место!</h1>
+            <div className='block_title_form'>
+              <h1 className='title_form'>Забронируй место!</h1>
+            </div>
 
             <div className='block_input_date'>
               <div className='block_input_date-input'>
@@ -41,19 +48,42 @@ export default function NotFound() {
                 <input className='input_error date' type="text" placeholder='Время'/>
               </div>
 
-              <div>
-                <div>
-                  <span>Количетсво гостей</span>
-                  <input className='input_error' type="text" placeholder='2'/>
+              <div className='block_input_other'>
+                <div className='contant_input_other'>
+                  <span className='input_other_quantity'>Количетсво гостей</span>
+                  <input className='input_error num' type="text" placeholder='2'/>
                 </div>
 
-                <div>
+                <div className='block_input_number_wishes'>
                   <input className='input_error' type="text" placeholder='Номер телефона'/>
-                  <input className='input_error' type="text" placeholder='Ваши пожелания'/>
+                  <textarea className='input_error textarea' name="textarea" placeholder='Ваши пожелания' cols={25} rows={4}/>
                 </div>
+
+                <div className='block_body_armor'>
+                  <ArrowButtomBtnSVG/>  
+
+                  <Image
+                  className='body_armor'
+                  src={'/static/img/errorIcon/bodyArmor.png'}
+                  width={250}
+                  height={280}
+                  alt='body armor'
+                  />
+
+                  <Image
+                  className='helmet'
+                  src={'/static/img/errorIcon/helmet.png'}
+                  width={140}
+                  height={140}
+                  alt='body armor'
+                  />
+
+                  <ArrowLeftBtnSVG/>
+                </div>
+                
               </div>
 
-              <button>Забронировать!</button>
+              <ButtonBookUI/>
             </div>
           </div>
         </div>
