17c11e00c035000d43a96eb3ac079fee9b407820
Anzor <anzor.shadzhe@gmail.com>
2023-10-01 21:33:44 +0300

    modified:   src/components/pagePriceList/index.tsx

--- src/components/pagePriceList/index.tsx
+++ src/components/pagePriceList/index.tsx
@@ -7,6 +7,7 @@ import { IPropsPageClubAbout } from "@/types/types";
 import { FC } from 'react'
 
 const PagePriceList: FC<IPropsPageClubAbout> = ({club}) => {
+    console.log(club);
     return (
         <main>
             <section className="container_price_list_standart">
